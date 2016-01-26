035814cbf07c4eacf255dc37b8e551ef5ee54702
dpryan79 <dpryan79@gmail.com>
2016-01-26 10:17:09 +0100

    Modify cmpAlignments, add compareBAMs and compareBigWigs.py

--- cmpAlignments/README.md
+++ cmpAlignments/README.md
@@ -1,8 +1,6 @@
-Compare two BAM files holding alignments of the same reads to different genomes, assigning the alignments to each genome (or none) according to their alignment score. Only single-end files are supported, since this is meant to be used to follow-up fastq_screen results. There is very little error checking performed and only the MAPQ is altered.
+Compare two BAM files holding alignments of the same reads to different genomes, assigning the alignments to each genome (or none) according to their alignment score. An inter-file MAPQ threshold can be specified and if the inter-file MAPQ is not at least it then the alignments are ignored. Both paired-end and single-end experiments are supported. There is very little error checking performed.
 
 The alignments *must* be made with bowtie2. The BAM files *must* have alignments in the same order.
-
-Note that the output files are sorted, so you can directly index and load them in IGV.
 
 Compilation
 ===========
@@ -14,4 +12,6 @@ Usage
 
     ./cmpAlignments file1.bam file2.bam
 
-This will write alignments to `file1.unique.bam` and `file2.unique.bam`. The MAPQ scores will be recalculated.
+This will write alignments to `file1.unique.bam` and `file2.unique.bam`. To specify an inter-file MAPQ threshold (the default is 0), use the `-q` option:
+
+    ./cmpAlignments -q 5 file1.bam file2.bam
--- cmpAlignments/cmpAlignments.c
+++ cmpAlignments/cmpAlignments.c
@@ -4,18 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
-#define kroundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+#include <getopt.h>
 
 int32_t getAS(bam1_t *b) {
     int32_t def = -1073741823; //half of INT_MIN
     uint8_t *p = bam_aux_get(b, "AS");
-    if(!p) return def;
-    return bam_aux2i(p);
-}
-
-int32_t getXS(bam1_t *b) {
-    int32_t def = -1073741823;
-    uint8_t *p = bam_aux_get(b, "XS");
     if(!p) return def;
     return bam_aux2i(p);
 }
@@ -130,47 +123,48 @@ int32_t calcMAPQ(int32_t AS, int32_t XS, int32_t scMin) {
     }
 }
 
-int cmpAl(const void *a, const void *b) {
-    bam1_t *b1 = *(bam1_t**)a;
-    bam1_t *b2 = *(bam1_t**)b;
-
-    if(b1->core.tid < b2->core.tid) return -1;
-    if(b1->core.tid > b2->core.tid) return 1;
-    if(b1->core.pos < b2->core.pos) return -1;
-    if(b1->core.pos > b2->core.pos) return 1;
-    if(bam_endpos(b1) < bam_endpos(b2)) return -1;
-    if(bam_endpos(b1) > bam_endpos(b2)) return 1;
-    return 0;
-}
-
-void sortAlignments(bam1_t **b, int32_t l) {
-    qsort((void*)b, l, sizeof(bam1_t*), &cmpAl);
-}
-
-void writeAlignments(htsFile *o, bam_hdr_t *hdr, bam1_t **b, int32_t l) {
-    int32_t i;
-    for(i=0; i<l; i++) assert(sam_write1(o, hdr, b[i]));
+void usage(char *prog) {
+    printf("Usage: %s [OPTIONS] file1.bam file2.bam\n", prog);
+    printf("Alignments in each file are compared, line by line, and those pairs with an\n"
+           "INTER-file MAPQ above threshold are written to file1.unique.bam or\n"
+           "file2.unique.bam, as appropriate.\n"
+           "\nOptions:\n"
+           " -q INT  The inter-file MAPQ threshold to write a pair to the output. Default 0.\n"
+           " -h      Print this help message.\n");
 }
 
 int main(int argc, char *argv[]) {
     htsFile *i1, *i2, *o1, *o2, *o;
-    bam1_t *r1, *r2;
-    bam1_t **r1s = NULL, **r2s = NULL;
+    bam1_t *r1_1, *r1_2, *r2_1, *r2_2;
     bam_hdr_t *hdr1, *hdr2;
-    int32_t AS1, AS2, XS1, XS2, l1 = 0, l2 = 0, m1 = 0, m2 = 0;
-    int32_t bestAS, bestXS, MAPQ, i;
-    int32_t scMin = -1073741823;
+    int32_t AS1_1, AS1_2, AS2_1, AS2_2, AS, XS;
+    int32_t scMin1 = -1073741823;
+    int32_t scMin2 = -1073741823;
     char str[1024], *p;
+    int opt, minMAPQ = 0;
 
-    if(argc != 3) {
-        fprintf(stderr, "Usage: %s file1.bam file2.bam\n", argv[0]);
-        fprintf(stderr, "Unique alignments with recalculated MAPQ scores will be written to file1.unique.bam and file2.unique.bam\n");
+    while((opt = getopt(argc, argv, "hq:")) != -1) {
+        switch(opt) {
+        case 'h':
+            usage(argv[0]);
+            return 0;
+        case 'q':
+            minMAPQ = atoi(optarg);
+            break;
+        default:
+            usage(argv[0]);
+            return 1;
+        }
+    }
+
+    if(argc-optind != 2) {
+        usage(argv[0]);
         return 0;
     }
 
-    i1 = sam_open(argv[1], "rb");
+    i1 = sam_open(argv[optind], "r");
     assert(i1);
-    i2 = sam_open(argv[2], "rb");
+    i2 = sam_open(argv[optind+1], "r");
     assert(i2);
 
     hdr1 = sam_hdr_read(i1);
@@ -178,85 +172,81 @@ int main(int argc, char *argv[]) {
     hdr2 = sam_hdr_read(i2);
     assert(hdr2);
 
-    r1 = bam_init1();
-    r2 = bam_init1();
-    assert(r1);
-    assert(r2);
+    r1_1 = bam_init1();
+    r1_2 = bam_init1();
+    r2_1 = bam_init1();
+    r2_2 = bam_init1();
+    assert(r1_1);
+    assert(r1_2);
+    assert(r2_1);
+    assert(r2_2);
 
     //create the output file
-    p = strrchr(argv[1], '.');
+    p = strrchr(argv[optind], '.');
     if(p) p[0] = '\0';
-    p = strrchr(argv[2], '.');
+    p = strrchr(argv[optind+1], '.');
     if(p) p[0] = '\0';
-    snprintf(str, 1024, "%s.unique.bam", argv[1]);
+    snprintf(str, 1024, "%s.unique.bam", argv[optind]);
     o1 = sam_open(str, "wb");
-    snprintf(str, 1024, "%s.unique.bam", argv[2]);
+    snprintf(str, 1024, "%s.unique.bam", argv[optind+1]);
     o2 = sam_open(str, "wb");
 
     assert(sam_hdr_write(o1, hdr1) == 0);
     assert(sam_hdr_write(o2, hdr2) == 0);
 
-    while(sam_read1(i1, hdr1, r1) > 0) {
-        assert(sam_read1(i2, hdr2, r2) > 0);
+    while(sam_read1(i1, hdr1, r1_1) > 0) {
+        if(r1_1->core.flag & 1) assert(sam_read1(i1, hdr1, r1_2));
+
+        assert(sam_read1(i2, hdr2, r2_1) > 0);
+        if(r2_1->core.flag & 1) assert(sam_read1(i2, hdr2, r2_2));
 
         //Make sure the names match
-        assert(strcmp(bam_get_qname(r1), bam_get_qname(r2)) == 0);
+        assert(strcmp(bam_get_qname(r1_1), bam_get_qname(r2_1)) == 0);
+        if(r1_1->core.flag & 1) {
+            assert(strcmp(bam_get_qname(r1_1), bam_get_qname(r1_2)) == 0);
+            assert(strcmp(bam_get_qname(r1_1), bam_get_qname(r2_2)) == 0);
+        }
 
         //Get the scores
-        if(scMin == -1073741823) scMin = calcScMin(r1);
-        AS1 = getAS(r1);
-        AS2 = getAS(r2);
-        XS1 = getXS(r1);
-        XS2 = getXS(r2);
+        scMin1 = calcScMin(r1_1);
+        scMin2 = 0;
+        if(r1_1->core.flag & 1) scMin2 = calcScMin(r1_2);
+        AS1_1 = getAS(r1_1);
+        AS2_1 = getAS(r2_1);
+        if(r1_1->core.flag & 1) {
+            AS1_2 = getAS(r1_2);
+            AS2_2 = getAS(r2_2);
+        } else {
+            AS1_2 = AS2_2 = 0;
+        }
 
-        if(AS1 > AS2) {//Assign to o1
-            bestAS = AS1;
-            if(XS1 > AS2) bestXS = XS1;
-            else bestXS = AS2;
+        if(AS1_1+AS1_2 > AS2_1+AS2_2) {//Assign to o1
+            AS = AS1_1 + AS1_2;
+            XS = AS2_1 + AS2_2;
             o = o1;
-        } else if(AS2 > AS1) { //Assign to o2
-            bestAS = AS2;
-            if(AS1 > XS2) bestXS = AS1;
-            else bestXS = XS2;
+        } else if(AS2_1+AS2_2 > AS1_1+AS1_2) { //Assign to o2
+            AS = AS2_1 + AS2_2;
+            XS = AS1_1 + AS1_2;
             o = o2;
         } else {
             continue;
         }
 
-        MAPQ = calcMAPQ(bestAS, bestXS, scMin);
-        if(o == o1) {
-            r1->core.qual = MAPQ;
-            if(l1+1 >= m1) {
-                m1 = l1+1;
-                kroundup32(m1);
-                assert((r1s = realloc(r1s, sizeof(bam1_t*)*m1)));
+        if(calcMAPQ(AS, XS, scMin1+scMin2) >= minMAPQ) {
+            if(o == o1) {
+                assert(sam_write1(o, hdr1, r1_1));
+                if(r1_1->core.flag & 1) assert(sam_write1(o, hdr1, r1_2));
+            } else {
+                assert(sam_write1(o, hdr2, r2_1));
+                if(r2_1->core.flag & 1) assert(sam_write1(o, hdr2, r2_2));
             }
-            assert(r1s);
-            r1s[l1++] = bam_dup1(r1);
-        } else {
-            r2->core.qual = MAPQ;
-            if(l2+1 >= m2) {
-                m2 = l2+1;
-                kroundup32(m2);
-                assert((r2s = realloc(r2s, sizeof(bam1_t*)*m2)));
-            }
-            assert(r2s);
-            r2s[l2++] = bam_dup1(r2);
         }
     }
 
-    sortAlignments(r1s, l1);
-    sortAlignments(r2s, l2);
-    writeAlignments(o1, hdr1, r1s, l1);
-    writeAlignments(o2, hdr2, r2s, l2);
-
-    for(i=0; i<l1; i++) bam_destroy1(r1s[i]);
-    for(i=0; i<l2; i++) bam_destroy1(r2s[i]);
-    if(r1s) free(r1s);
-    if(r2s) free(r2s);
-
-    bam_destroy1(r1);
-    bam_destroy1(r2);
+    bam_destroy1(r1_1);
+    bam_destroy1(r1_2);
+    bam_destroy1(r2_1);
+    bam_destroy1(r2_2);
     bam_hdr_destroy(hdr1);
     bam_hdr_destroy(hdr2);
     sam_close(o1);
